4b80147432b4f9bf16e8528ba80be6346fad0ce3
odinokun <odinokun@gmail.com>
2018-07-25 14:50:30 +0300

    add 03-Теория. Vue CLI&Webpack

---
+++ 03 - Теория. Vue CLI&Webpack/02 - Установка CLI и создание проекта/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
