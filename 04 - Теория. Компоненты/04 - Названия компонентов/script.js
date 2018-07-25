fbf8abadce60e534f65cd040b2a1d838aa1cb6e7
odinokun <odinokun@gmail.com>
2018-07-25 19:24:15 +0300

    add 4-4

---
+++ 04 - Теория. Компоненты/04 - Названия компонентов/script.js
@@ -0,0 +1,32 @@
+var component = {
+  template: `
+    <div class="wrap">
+      <h2>Counter: {{ counter }}</h2>
+      <button @click="add">Add to counter</button>
+    </div>
+  `,
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    add: function() {
+      this.counter++
+    }
+  }
+}
+
+new Vue({
+  el:'#app',
+  components: {
+    'my-counter': component
+  }
+});
+
+new Vue({
+  el:'#app2',
+  components: {
+    myCounter: component
+  }
+});
