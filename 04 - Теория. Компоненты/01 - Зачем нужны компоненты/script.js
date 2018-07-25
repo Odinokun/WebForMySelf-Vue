f793c1eff2d78b9ed3b1c9c1bc73a15692979043
odinokun <odinokun@gmail.com>
2018-07-25 17:59:45 +0300

    04-01 зачем нужны компоненты

---
+++ 04 - Теория. Компоненты/01 - Зачем нужны компоненты/script.js
@@ -0,0 +1,22 @@
+Vue.component('my-counter', {
+  template: `
+    <div class="wrap">
+      <h2>Counter: {{ counter }}</h2>
+      <button @click="add">Add to counter</button>
+    </div>
+  `,
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    add: function() {
+      this.counter++
+    }
+  }
+})
+
+new Vue({
+  el:'#app'
+});
