ee2ef75ffa8a92d1119ce90e2a787b6ca4c3c99d
odinokun <odinokun@gmail.com>
2018-07-26 22:44:43 +0300

    did lesson 05-01

---
+++ 05 - Теория. Директивы/01 - Создание своей директивы/block-05/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
